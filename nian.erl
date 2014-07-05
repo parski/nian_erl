f881a4352075d8fb9d7e190749776c74c41970c7
Pär Strindevall <par@strindevall.com>
2014-07-05 14:06:04 +0200

    Timer added.

--- nian.erl
+++ nian.erl
@@ -28,13 +28,13 @@ test_solve() ->
 
 % Type specification
 
--spec test() -> none().
+-spec test() -> 'pass'.
 -spec test_check_word() -> 'pass'.
--spec test_solve() -> none().
--spec solve([byte()] | char()) -> 'ok'.
--spec start(non_neg_integer(),[any()]) -> 'false' | 'ok'.
--spec reader(pid() | {'file_descriptor',atom() | tuple(),_},[byte()] | char(),[string()]) -> 'ok'.
--spec done([string()],[byte()] | char()) -> 'ok'.
+-spec test_solve() -> 'pass'.
+-spec solve([any()]) -> 'false' | 'ok'.
+-spec start(non_neg_integer(),[any()],{non_neg_integer(),non_neg_integer(),non_neg_integer()}) -> 'false' | 'ok'.
+-spec reader(pid() | {'file_descriptor',atom() | tuple(),_},[byte()],[string()],{non_neg_integer(),non_neg_integer(),non_neg_integer()}) -> 'ok'.
+-spec done([string()],[byte()],{non_neg_integer(),non_neg_integer(),non_neg_integer()}) -> 'ok'.
 -spec check_critical(string(),[byte(),...],[string()]) -> [string()].
 -spec check_critical_result(boolean(),string(),[byte(),...],[string()]) -> [string()].
 -spec check_word(boolean(),string(),[byte()]) -> boolean().
@@ -43,32 +43,34 @@ test_solve() ->
 % Module functions
 
 -define(CHARCOUNT, 9). % Change this macro value to turn Nian into Elvan, ... , N-an.
-% If the CHARCOUNT value is changed the check_critical/2 function must be changed. This could probably be done with lists:nth((?CHARCOUNT - 1) / 2 + 1, Input) for uneven CHARCOUNT values but I try to avoid BIFs. The arithmetics will most likely evaluate with a decimal digit so formatting would need some tinkering.
+% If the CHARCOUNT value is changed the check_critical/3 function must be changed. This could probably be done with lists:nth((?CHARCOUNT - 1) / 2 + 1, Input) for uneven CHARCOUNT values but I try to avoid BIFs. The arithmetics will most likely evaluate with a decimal digit so formatting would need some tinkering.
 
 % solve/1, the landing function. Feed it strings, please.
 solve(Input) ->
-	start(length(Input), Input).
+	StartTime = now(),
+	start(length(Input), Input, StartTime).
 
-start(?CHARCOUNT, Input) ->
+start(?CHARCOUNT, Input, StartTime) ->
 	{ok, IO} = file:open("svenskaOrd.txt", [read, raw, read_ahead]),
-	reader(IO, string:to_lower(Input), []);
+	reader(IO, string:to_lower(Input), [], StartTime);
 	
-start(_, _) ->
+start(_, _, _) ->
 	false.
 
-reader(File, Checklist, Truths) ->
+reader(File, Checklist, Truths, StartTime) ->
 	case file:read_line(File) of
 		{ok, Line} ->
 			Word = string:strip(Line, both, $\n),
 			Truths1 = check_critical(Word, Checklist, Truths),
-			reader(File, Checklist, Truths1);
+			reader(File, Checklist, Truths1, StartTime);
 		eof ->
-			done(Truths, Checklist)
+			done(Truths, Checklist, StartTime)
 		end.
 	
-done(Truths, Checklist) ->
+done(Truths, Checklist, StartTime) ->
+	Time = timer:now_diff(now(), StartTime),
 	Anagrams = [X || X <- Truths, length(X) == 9],
-	io:format("~p matches for input ~p:~n~n~p~n~nAnagrams:~n~n~p~n",[length(Truths), Checklist,Truths, Anagrams]).
+	io:format("~n~p matches for input ~p in ~p microseconds:~n~n~p~n~nAnagrams:~n~n~p~n~n",[length(Truths), Checklist, Time, Truths, Anagrams]).
 
 % check_critical/3 checks if the current word contains the critical letter in Checklist.
 check_critical(Word, Checklist, Truths) ->
